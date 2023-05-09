3b3adffcb3c91bf8cdd784e50742922131dceaf5
Wesley Peters <wesleypeters@gmail.com>
2023-05-09 14:14:23 -0600

    created array and added and removed elements destructively and nondestructively

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+
+function appendCat(name) {
+
+    const newCats = [...cats, name];
+    return newCats;
+};
+
+function prependCat(name) {
+    const moreNewCats = [name, ...cats];
+    return moreNewCats;
+};
+
+function removeLastCat() {
+   return cats.slice(0, -1);
+};
+
+function removeFirstCat() {
+    return cats.slice(1);
+};
